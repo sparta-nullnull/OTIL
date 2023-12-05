dcb3360819c36f85f52c5f2d1c813326d0f14e3e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-12-05 19:24:30 +0900

    init project

---
+++ src/main/java/com/spartanullnull/otil/OtilApplication.java
@@ -0,0 +1,13 @@
+package com.spartanullnull.otil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartanullnull/otil/OtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartanullnull.otil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
