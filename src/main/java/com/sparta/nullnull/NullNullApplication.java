3f6dc0111bddfd343bc3630e1856203db0099619
JaeHoon <wogns8030@naver.com>
2023-12-06 14:03:50 +0900

    feat : Signup, Login

---
+++ src/main/java/com/sparta/nullnull/NullNullApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nullnull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullNullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NullNullApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sparta/nullnull/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.sparta.nullnull.user.entity;
+
+import com.sparta.nullnull.user.userenum.UserRoleEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column
+    @NotNull
+    private String password;
+
+    @Column(nullable = false,unique = true)
+    @NotNull
+    private String nickname;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, String nickname, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+        this.role = role;
+    }
+}
