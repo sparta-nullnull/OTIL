764a83e4799135e9e380980f3c901a5c7b5f6a45
gyutae <qoxowkd0716@gmail.com>
2023-12-06 20:18:38 +0900

    refactor: @Setter 삭제 및 @RequiredArgsConstructor 추가, @Builder 패턴 적용

--- src/main/java/com/spartanullnull/otil/comment/dto/CommentResponseDto.java
+++ src/main/java/com/spartanullnull/otil/comment/dto/CommentResponseDto.java
@@ -1,37 +1,27 @@
 package com.spartanullnull.otil.comment.dto;
 
 import com.spartanullnull.otil.comment.entity.Comment;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 public class CommentResponseDto { // 클라이언트에게 응답할 때 반환하는 데이터를 정의
-    private Long id;
-    private String commentText;
-    private Long userId; // 사용자 ID
-    private String username; // 사용자 이름
+    private final Long id;
+    private final String commentText;   // 댓글 텍스트
+    private final Long userId;          // 사용자 ID
+    private final String username;      // 사용자 이름
+    private final Long postId;          // TIL ID
 
-    private Long postId; // 게시물 ID
 
-    // 생성자
-    public CommentResponseDto(Long id, String commentText, Long userId, String username,
-        Long postId) {
-        this.id = id;
-        this.commentText = commentText;
-        this.userId = userId;
-        this.username = username;
-        this.postId = postId;
-    }
-
-    // Entity에서 Dto로 변환하는 메서드
+    // Comment Entity에서 CommentResponseDto로 변환하는 메서드
     public static CommentResponseDto fromEntity(Comment comment) {
-        return new CommentResponseDto(
-            comment.getId(),
-            comment.getCommentText(),
-            comment.getUser().getId(),
-            comment.getUser().getUsername(),
-            comment.getPost().getId()
-        );
+        return CommentResponseDto.builder()
+            .id(comment.getId())
+            .commentText(comment.getCommentText())
+            .userId(comment.getId())
+            .username(comment.getUser().getUsername)
+            .postId(comment.getPost().getId())
+            .build();
     }
 }
