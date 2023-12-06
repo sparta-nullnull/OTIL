af117d6943e1a6d3995a2f609a8b2c0b54ab5f80
JaeHoon <wogns8030@naver.com>
2023-12-06 19:55:26 +0900

    feat : kakaoLogin, logout

--- src/main/java/com/sparta/nullnull/user/entity/User.java
+++ src/main/java/com/sparta/nullnull/user/entity/User.java
@@ -16,7 +16,6 @@ import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table
 public class User {
@@ -43,11 +42,26 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
+    private Long kakaoId;
+
     public User(String accountId, String password, String email, String nickname, UserRoleEnum role) {
         this.accountId = accountId;
         this.password = password;
         this.nickname = nickname;
         this.email = email;
         this.role = role;
+    }
+
+    public User(Long id, String encodedPassword, String email, String nickname, UserRoleEnum role) {
+        this.kakaoId = id;
+        this.password = encodedPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
     }
 }
