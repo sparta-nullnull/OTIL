d894003def3d649581455b6e882e27bd407c5554
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-12-07 10:59:53 +0900

    merge: merge conflict

--- src/main/java/com/spartanullnull/otil/OtilApplication.java
+++ src/main/java/com/spartanullnull/otil/OtilApplication.java
@@ -1,14 +1,13 @@
 package com.spartanullnull.otil;
 
-import com.spartanullnull.otil.user.entity.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OtilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OtilApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OtilApplication.class, args);
+    }
 
 }
--- src/main/java/com/spartanullnull/otil/post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spartanullnull.otil.post.repository;
-
-import com.spartanullnull.otil.post.entity.*;
-import org.springframework.data.jpa.repository.*;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
