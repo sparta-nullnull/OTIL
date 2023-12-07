e9717b62fe812469f56d0e723a17c2433e2b03ce
gyutae <qoxowkd0716@gmail.com>
2023-12-07 16:46:07 +0900

    feat: LikeRepository 기능 구현

---
+++ src/main/java/com/spartanullnull/otil/domain/like/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.spartanullnull.otil.domain.like.repository;
+
+import com.spartanullnull.otil.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+}
