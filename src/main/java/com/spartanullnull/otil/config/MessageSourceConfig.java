7d0c2e64c5c4860de1488a77cf21d0a1b92b5c48
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-12-07 18:12:40 +0900

    feat: MessageSource 빈 생성 Configuration 구현

---
+++ src/main/java/com/spartanullnull/otil/config/MessageSourceConfig.java
@@ -0,0 +1,58 @@
+package com.spartanullnull.otil.config;
+
+import java.util.*;
+import net.rakugakibox.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.*;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.*;
+import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.*;
+
+@Configuration
+public class MessageSourceConfig implements WebMvcConfigurer {
+
+    @Bean // 세션에 지역설정. default는 KOREAN = 'ko'
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.KOREAN);
+        return slr;
+    }
+
+    @Bean // 지역설정을 변경하는 인터셉터. 요청시 파라미터에 lang 정보를 지정하면 언어가 변경됨.
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        lci.setIgnoreInvalidLocale(true);
+        return lci;
+    }
+
+    @Override // 인터셉터를 시스템 레지스트리에 등록
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean // yml 파일을 참조하는 MessageSource 선언
+    public MessageSource messageSource(
+        @Value("i18n/exception") String basename,
+        @Value("UTF-8") String encoding
+    ) {
+        YamlMessageSource ms = new YamlMessageSource();
+        ms.setBasename(basename);
+        ms.setDefaultEncoding(encoding);
+        ms.setAlwaysUseMessageFormat(true);
+        ms.setUseCodeAsDefaultMessage(true);
+        ms.setFallbackToSystemLocale(true);
+        return ms;
+    }
+
+    // locale 정보에 따라 다른 yml 파일을 읽도록 처리
+    private static class YamlMessageSource extends ResourceBundleMessageSource {
+
+        @Override
+        protected ResourceBundle doGetBundle(String basename, Locale locale) {
+            return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
+        }
+    }
+}
