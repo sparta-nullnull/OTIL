927656af4642517809e62666dc930d8a4a7cb57b
gyutae <qoxowkd0716@gmail.com>
2023-12-09 01:19:14 +0900

    feat: 댓글 좋아요 Service 구현

--- src/main/java/com/spartanullnull/otil/domain/like/service/LikeService.java
+++ src/main/java/com/spartanullnull/otil/domain/like/service/LikeService.java
@@ -1,5 +1,8 @@
 package com.spartanullnull.otil.domain.like.service;
 
+import com.spartanullnull.otil.domain.comment.controller.CommentController;
+import com.spartanullnull.otil.domain.comment.entity.Comment;
+import com.spartanullnull.otil.domain.comment.repository.CommentRepository;
 import com.spartanullnull.otil.domain.like.entity.Like;
 import com.spartanullnull.otil.domain.like.repository.LikeRepository;
 import com.spartanullnull.otil.domain.post.entity.Post;
@@ -21,6 +24,7 @@ public class LikeService {
     private final LikeRepository likeRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
 
     /**
      * 사용자가 TIL에 좋아요를 누르는 메서드
@@ -93,5 +97,57 @@ public class LikeService {
             top3Posts.add(post);
         }
         return top3Posts;
+    }
+
+    /**
+     * 사용자가 댓글을 좋아요를 누르는 메서드
+     *
+     * @param userId        좋아요를 누르는 사용자
+     * @param commentId     좋아요를 누를 댓글 ID
+     */
+    @Transactional
+    public void createLikeForComment(Long userId, Long commentId) {
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
+
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다."));
+
+        if (likeRepository.existsByUserAndComment(user, comment)) {
+            // 좋아요가 이미 존재하면 처리하지 않음
+            return;
+        }
+
+        likeRepository.save(Like.builder().user(user).comment(comment).build());
+    }
+
+    /**
+     * 사용자가 댓글에 좋아요를 취소하는 메서드
+     *
+     * @param userId      좋아요 취소하는 사용자
+     * @param commentId   좋아요를 취소할 댓글 ID
+     */
+    @Transactional
+    public void deleteLikeForComment(Long userId, Long commentId) {
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
+
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다."));
+
+        if (likeRepository.existsByUserAndComment(user, comment)) {
+            likeRepository.deleteByUserAndComment(user, comment);
+        }
+    }
+
+    /**
+     *
+     * @param commentId   조회할 댓글 ID
+     * @return            댓글의 좋아요 개수
+     */
+    public Long getLikeCountForComment(Long commentId) {
+        return likeRepository.countByCommentId(commentId);
     }
 }
