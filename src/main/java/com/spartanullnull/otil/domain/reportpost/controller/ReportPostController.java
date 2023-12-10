af26bbe5197c7174cb408f2d585ff99d60b02324
JaeHoon <wogns8030@naver.com>
2023-12-10 17:38:36 +0900

    feat : 권한 별 조회

--- src/main/java/com/spartanullnull/otil/domain/reportpost/controller/ReportPostController.java
+++ src/main/java/com/spartanullnull/otil/domain/reportpost/controller/ReportPostController.java
@@ -3,6 +3,7 @@ package com.spartanullnull.otil.domain.reportpost.controller;
 import com.spartanullnull.otil.domain.reportpost.service.ReportPostService;
 import com.spartanullnull.otil.domain.reportpost.dto.ReportPostRequestDto;
 import com.spartanullnull.otil.domain.reportpost.dto.ReportPostResponseDto;
+import com.spartanullnull.otil.domain.user.entity.UserRoleEnum;
 import com.spartanullnull.otil.global.dto.CommonResponseDto;
 import com.spartanullnull.otil.security.Impl.UserDetailsImpl;
 import java.util.concurrent.RejectedExecutionException;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportPostController {
 
     private final ReportPostService reportPostService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public ResponseEntity<ReportPostResponseDto> createReport(
@@ -37,22 +40,41 @@ public class ReportPostController {
                 userDetails.getUser());
             return ResponseEntity.ok().body(reportPostResponseDto);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.badRequest().build();
         }
     }
 
-    @GetMapping("/gets")
-    public Page<ReportPostResponseDto> getAllReport(
+    @GetMapping("/gets/user")
+    public ResponseEntity<Page<ReportPostResponseDto>> getUserAllReportPost(
         @RequestParam("page") int page,
         @RequestParam("size") int size,
         @RequestParam("sortBy") String sortBy,
         @RequestParam("isAsc") boolean isAsc,
         @RequestBody ReportPostRequestDto requestDto,
         @AuthenticationPrincipal UserDetailsImpl user) {
-        String password = requestDto.getPassword();
-        return reportPostService.getAllNewsFeeds(password, user.getUser(), page - 1,
-            size, sortBy,
-            isAsc);
+        System.out.println(user.getUser().getRole());
+        if (isValidUser(user, requestDto) && user.getUser().getRole() == UserRoleEnum.USER) {
+            Page<ReportPostResponseDto> reports = reportPostService.getUserAllReportPost(user.getUser(), page - 1, size, sortBy, isAsc);
+            return ResponseEntity.ok().body(reports);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/gets/admin")
+    public ResponseEntity<Page<ReportPostResponseDto>> getAdminAllReportPost(
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc,
+        @RequestBody ReportPostRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl user) {
+        if (isValidAdmin(user, requestDto) && user.getUser().getRole() == UserRoleEnum.ADMIN) {
+            Page<ReportPostResponseDto> reports = reportPostService.getAdminAllReportPost(page - 1, size, sortBy, isAsc);
+            return ResponseEntity.ok().body(reports);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/get/{postid}")
@@ -89,5 +111,19 @@ public class ReportPostController {
             return ResponseEntity.badRequest()
                 .body(new CommonResponseDto("삭제 실패", HttpStatus.BAD_REQUEST.value()));
         }
+    }
+
+    private boolean isValidUser(UserDetailsImpl user, ReportPostRequestDto requestDto) {
+        String encodePassword = user.getPassword();
+        return isValidCredentials(requestDto.getPassword(),encodePassword);
+    }
+
+    private boolean isValidAdmin(UserDetailsImpl user, ReportPostRequestDto requestDto) {
+        String encodePassword = user.getPassword();
+        return isValidCredentials(requestDto.getPassword(),encodePassword);
+    }
+
+    private boolean isValidCredentials(String storedPassword, String providedPassword) {
+        return passwordEncoder.matches(storedPassword, providedPassword);
     }
 }
--- src/main/java/com/spartanullnull/otil/domain/reportpost/repository/ReportPostRepository.java
+++ src/main/java/com/spartanullnull/otil/domain/reportpost/repository/ReportPostRepository.java
@@ -1,9 +1,14 @@
 package com.spartanullnull.otil.domain.reportpost.repository;
 
 import com.spartanullnull.otil.domain.reportpost.entity.ReportPost;
+import com.spartanullnull.otil.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportPostRepository extends JpaRepository<ReportPost,Long> {
+
+    Page<ReportPost> findByUser(User user, Pageable pageable);
 }
